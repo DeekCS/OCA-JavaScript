0714a4c4ed9a0d124059a6f9455d4a6bbe9cc7ac
DeekCS <31720001079@std.bau.edu.jo>
2021-10-13 22:27:46 +0300

    playlist advance

---
+++ Exercise (7B) - Advanced playlist/script.js
@@ -0,0 +1,23 @@
+let add = document.getElementById('addBtn');
+let del = document.getElementById('del');
+let playlist = document.getElementById('playlist');
+
+add.addEventListener('click', function() {
+    let newSong = document.createElement('div');
+        let songName=document.getElementById("songName");
+    playlist.style.display = 'block';
+    playlist.appendChild(newSong);
+    newSong.innerHTML = songName.value;
+
+   
+        let removeBtn = document.createElement('button');
+        removeBtn.innerHTML = 'Delete';
+        newSong.appendChild(removeBtn);
+        removeBtn.addEventListener('click', function() {
+            playlist.removeChild(newSong);
+        });
+
+   
+    
+});
+
