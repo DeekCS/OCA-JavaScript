0112d169971dd63623ac6b22472d27027a8e4a7c
DeekCS <31720001079@std.bau.edu.jo>
2021-10-13 14:16:58 +0300

    update

--- Exercise (1) - Changing text/script.js
+++ Exercise (1) - Changing text/script.js
@@ -1,7 +1,7 @@
 let yellow = document.getElementById("yellow");
 
-document.getElementById("yellow").onmouseover = function() {mouseOver()};
-document.getElementById("yellow").onmouseout = function() {mouseOut()};
+yellow.onmouseover = function() {mouseOver()};
+yellow.onmouseout = function() {mouseOut()};
 
 function mouseOver() {
    yellow.innerHTML="<h2>Can I help you?</h2>";
