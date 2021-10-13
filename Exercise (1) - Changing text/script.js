0a0feaa203c5442a1df9f6e1c8c168e034dfdcf5
DeekCS <31720001079@std.bau.edu.jo>
2021-10-13 09:52:26 +0300

    upload Ex-1

---
+++ Exercise (1) - Changing text/script.js
@@ -0,0 +1,14 @@
+let yellow = document.getElementById("yellow");
+
+document.getElementById("yellow").onmouseover = function() {mouseOver()};
+document.getElementById("yellow").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+   yellow.innerHTML="<h2>Can I help you?</h2>";
+}
+
+function mouseOut() {
+    yellow.innerHTML="<h2>Hello World</h2>  ";
+
+
+}
