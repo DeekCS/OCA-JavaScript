aa69845f076772f605a50ad07dcd471619bb7a0b
DeekCS <31720001079@std.bau.edu.jo>
2021-10-22 22:29:27 +0300

    Ex.3 Conditions

--- JavaScript Exercises/conditionals/conditionals.js
+++ JavaScript Exercises/conditionals/conditionals.js
@@ -46,7 +46,28 @@ let x =3, y=-7, z=2;
 Sample numbers : 0, -1, 4 
 Output : 4, 0, -1 */
 /******* Start Your Code *********/
-  
+        function sort(x,y,z) {
+        if (x > y && x > z) {
+            if (y > z) {
+                console.log( `${x} ${y} ${z}`);
+            }else {
+                console.log(`${x} ${z} ${y}`);
+            }
+        }else if (y>x && y>z) {
+            if (x > z) {
+                console.log(`${y} ${x} ${z}`);
+            }else{
+                console.log(`${y} ${z} ${x}`);
+            }
+        } else if (z>x && z>y) {
+            if (x>y){
+                console.log(`${z} ${x} ${y}`);
+            }else {
+                console.log(`${z} ${y} ${x}`);
+            }
+            }
+        }
+        sort(0,-1,4);
  /******* End Your Code ********* */
 
 
